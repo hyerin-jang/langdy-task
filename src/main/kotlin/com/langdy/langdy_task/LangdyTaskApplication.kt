d8a3dbdb7110303d029242ee9f46099ebf47663d
Rin <hrjang92@gmail.com>
2026-03-05 02:23:15 +0900

    Initial commit

---
+++ src/main/kotlin/com/langdy/langdy_task/LangdyTaskApplication.kt
@@ -0,0 +1,11 @@
+package com.langdy.langdy_task
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LangdyTaskApplication
+
+fun main(args: Array<String>) {
+	runApplication<LangdyTaskApplication>(*args)
+}
---
+++ src/test/kotlin/com/langdy/langdy_task/LangdyTaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.langdy.langdy_task
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LangdyTaskApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
